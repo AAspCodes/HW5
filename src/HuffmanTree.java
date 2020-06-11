43fddb459e00f441a162eab800a0076ed888a512
AAspCodes <asp.anthony1@gmail.com>
2020-06-11 13:38:52 -0700

    implement write/travwrite

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -14,6 +14,7 @@ public class HuffmanTree {
 	 * Writes the current tree to the given output stream in standard format.
 	 */
 	public void write(PrintStream output) {
+		tree.travWrite(output, "");
 	}
 
 	private class HuffmanNode implements Comparable<HuffmanNode> {
@@ -33,5 +34,20 @@ public class HuffmanTree {
 		public int compareTo(HuffmanNode that) {
 			return this.freq - that.freq;
 		}
+
+		private void travWrite(PrintStream output, String code) {
+			if (asciiVal != 0) {
+				output.println(asciiVal);
+				output.println(code);
+			} else {
+				if (left != null) {
+					left.travWrite(output, code + "0");
+				}
+				if (right != null) {
+					right.travWrite(output, code + "1");
+				}
+			}
+		}
+
 	}
 }

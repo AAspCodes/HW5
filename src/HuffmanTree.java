f211d04eb7870d6a50492db3787ed88212820f42
AAspCodes <asp.anthony1@gmail.com>
2020-06-11 13:37:15 -0700

    move comments to above methods

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,18 +1,18 @@
 import java.io.PrintStream;
 
 public class HuffmanTree {
+
+	/**
+	 * Constructs a Huffman tree using the given array of frequencies where count[i]
+	 * is the number of occurrences of the character with ASCII value i.
+	 */
 	public HuffmanTree(int[] count) {
-		/**
-		 * Constructs a Huffman tree using the given array of
-		 *  frequencies where count[i] is the number of occurrences
-		 *   of the character with ASCII value i.
-		 */
 	}
 	
+
+	/**
+	 * Writes the current tree to the given output stream in standard format.
+	 */
 	public void write(PrintStream output) {
-		/**
-		 * Writes the current tree to the given
-		 *  output stream in standard format.
-		 */
 	}
 }

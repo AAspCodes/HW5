18f43a4d02c1188913269715808e8f4f57a7b7af
AAspCodes <asp.anthony1@gmail.com>
2020-06-11 13:39:48 -0700

    implement HuffmanTree constructor

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,12 +1,48 @@
 import java.io.PrintStream;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class HuffmanTree {
+	private HuffmanNode tree;
 
 	/**
 	 * Constructs a Huffman tree using the given array of frequencies where count[i]
 	 * is the number of occurrences of the character with ASCII value i.
 	 */
 	public HuffmanTree(int[] count) {
+
+		Queue<HuffmanNode> nodeQ = new PriorityQueue<HuffmanNode>();
+
+		// assign non zero frequency values from the count array to the priority queue
+		for (int i = 0; i < count.length; i++) {
+			if (count[i] != 0) {
+				nodeQ.add(new HuffmanNode(i, count[i]));
+			}
+		}
+
+		// add end of file char.
+		nodeQ.add(new HuffmanNode(count.length, 1));
+
+		// build tree
+		while (nodeQ.size() > 1) {
+			HuffmanNode leaf1 = nodeQ.remove();
+			HuffmanNode leaf2 = nodeQ.remove();
+			HuffmanNode branch = new HuffmanNode(0, leaf1.freq + leaf2.freq);
+
+			if (leaf1.freq <= leaf2.freq) {
+				branch.left = leaf1;
+				branch.right = leaf2;
+			} else {
+				branch.left = leaf2;
+				branch.right = leaf1;
+			}
+
+			nodeQ.add(branch);
+		}
+
+		// assign tree
+		this.tree = nodeQ.remove();
+
 	}
 	
 

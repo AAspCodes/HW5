2c25b1a401605b306c9506fc40db393ce8373c87
AAspCodes <asp.anthony1@gmail.com>
2020-06-12 15:23:19 -0700

    remove unused imports

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,8 +1,4 @@
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
